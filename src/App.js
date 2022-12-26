cd83b5f3155481ca632a292f2689dc0845e4c268
= <elijahmuraoka03@gmail.com>
2022-12-25 16:26:33 -1000

    init movie library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./components/MovieCard.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const API_KEY = "a2723861";
+    const API_URL = `http://omdbapi.com?apikey=${API_KEY}`;
+
+    const searchMovies = async (title) => {
+        await fetch(`${API_URL}&s=${title}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Unable to retrieve data properly.");
+                } else {
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                console.log("Found movie data successfully: ", data.Search);
+                setMovies(data.Search);
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    };
+
+    const handleEnterKey = (e) => {
+        if (e.key === "Enter") {
+            console.log("Search icon was clicked with value: ", searchQuery);
+            setSearchQuery("");
+            searchMovies(searchQuery);
+        }
+    };
+
+    const [movies, setMovies] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    useEffect(() => {
+        searchMovies("Batman");
+    }, []);
+
+    return (
+        <div id="app">
+            <h1>MovieLibrary</h1>
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder="Search for movies"
+                    value={searchQuery}
+                    onChange={(e) => {
+                        setSearchQuery(e.target.value);
+                    }}
+                    onKeyDown={handleEnterKey}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search icon"
+                    onClick={() => {
+                        console.log(
+                            "Search icon was clicked with value: ",
+                            searchQuery
+                        );
+                        setSearchQuery("");
+                        searchMovies(searchQuery);
+                    }}
+                />
+            </div>
+            {movies.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => {
+                        return <MovieCard key={movie.imdbID} movie={movie} />;
+                    })}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
